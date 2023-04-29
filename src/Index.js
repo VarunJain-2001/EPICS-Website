c7a147365fc54397bde0df446e8feb9aaa7e5506
Aaayush879 <aaayush879@gmail.com>
2023-04-29 09:59:33 +0530

    Add existing file

--- src/Index.js
+++ src/Index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
 const Register = require("./model/Register");
+const Query = require('./model/Query');
+const ContactUs = require('./model/Contactus');
 const port = process.env.PORT ||2000;
 const cors = require('cors');
+const Contactus = require("./model/Contactus");
 app.use(express.json());
 app.use(cors());
 mongoose.connect("mongodb+srv://ayushh:ayush@cluster0.yqrvv.mongodb.net/?retryWrites=true&w=majority",{
@@ -17,7 +20,7 @@ mongoose.connect("mongodb+srv://ayushh:ayush@cluster0.yqrvv.mongodb.net/?retryWr
     console.log(e);
 
 })
-app.use(cors());
+
 app.get('/',(req,res)=>{
     res.send("hi");
 })
@@ -52,6 +55,44 @@ app.post("/signin",async(req,res)=>{
     }
 
     
+})
+app.post('/query',async(req,res)=>{
+    const query = await new Query({
+        name:req.body.name,
+        email:req.body.email,
+        mobile:req.body.mobile,
+        regarding:req.body.regarding,
+        pickup:req.body.pickup,
+        subject:req.body.subject,
+        query:req.body.query
+    })
+    const insertQ = await query.save();
+    res.send(insertQ);
+    console.log(insertQ);
+})
+app.post('/contact',async(req,res)=>{
+    const contact = await new ContactUs({
+        name:req.body.name,
+        address:req.body.address,
+        mobile:req.body.mobile,
+        message:req.body.message,
+        donation:req.body.donation,
+    })
+    const insertQ = await contact.save();
+    res.send(insertQ);
+    console.log(insertQ);
+})
+app.get('/contactus',async(req,res)=>{
+    Contactus.find()
+    .then((document)=>{
+        res.send(document);
+    })
+})
+app.get('/Query',async(req,res)=>{
+    Query.find()
+    .then((document)=>{
+        res.send(document);
+    })
 })
 app.listen(port,(req,res)=>{
     console.log(`connected at ${port}`);
---
+++ src/model/Contactus.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const ContactusSchema = new mongoose.Schema({
+    name:{
+        type:String
+    },
+    address:{
+        type:String
+    },
+    mobile:{
+        type:Number,
+        maxlength:500 
+    },
+    message:{
+        type:String
+    },
+    donation:{
+        type:String
+    }
+   
+})
+const Contactus = new mongoose.model('Contactus',ContactusSchema);
+module.exports = Contactus;
